243450ecb45527e6679ae198f5d5662d50c8809c
uncletientrung <thihachcf@gmail.com>
2026-01-09 19:09:16 +0700

    database chua co du lieu ben trong

---
+++ config/quanlybangiay.sql
@@ -0,0 +1,426 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 09, 2026 lúc 01:08 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `quanlybangiay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admin`
+--
+
+CREATE TABLE `admin` (
+  `maadmin` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` int(50) NOT NULL,
+  `hoten` varchar(255) NOT NULL,
+  `sdt` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ctdonhang`
+--
+
+CREATE TABLE `ctdonhang` (
+  `madh` int(11) NOT NULL,
+  `masp` int(11) NOT NULL,
+  `masize` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `dongia` decimal(12,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ctphieunhap`
+--
+
+CREATE TABLE `ctphieunhap` (
+  `mapn` int(11) NOT NULL,
+  `masp` int(11) NOT NULL,
+  `masize` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `dongia` decimal(12,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `donhang`
+--
+
+CREATE TABLE `donhang` (
+  `madh` int(11) NOT NULL,
+  `makh` int(11) NOT NULL,
+  `tongtien` decimal(12,0) NOT NULL,
+  `thoigiantao` datetime NOT NULL DEFAULT current_timestamp(),
+  `diachigiaohang` varchar(255) NOT NULL,
+  `hinhthucthanhtoan` varchar(50) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `giohang`
+--
+
+CREATE TABLE `giohang` (
+  `makh` int(11) NOT NULL,
+  `masp` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hang`
+--
+
+CREATE TABLE `hang` (
+  `mahang` int(11) NOT NULL,
+  `tenhang` varchar(255) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hinhanh`
+--
+
+CREATE TABLE `hinhanh` (
+  `mahinhanh` int(11) NOT NULL,
+  `masp` int(11) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `ismain` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `makh` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `matkhau` varchar(50) NOT NULL,
+  `sdt` varchar(50) NOT NULL,
+  `hoten` varchar(255) NOT NULL,
+  `diachi` varchar(255) NOT NULL,
+  `gioitinh` int(1) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `loai`
+--
+
+CREATE TABLE `loai` (
+  `maloai` int(11) NOT NULL,
+  `tenloai` varchar(255) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `mau`
+--
+
+CREATE TABLE `mau` (
+  `mamau` int(11) NOT NULL,
+  `tenmau` varchar(255) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhacungcap`
+--
+
+CREATE TABLE `nhacungcap` (
+  `mancc` int(11) NOT NULL,
+  `tenncc` varchar(255) NOT NULL,
+  `sdt` varchar(50) NOT NULL,
+  `diachi` varchar(255) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phieunhap`
+--
+
+CREATE TABLE `phieunhap` (
+  `mapn` int(11) NOT NULL,
+  `maadmin` int(11) NOT NULL,
+  `mancc` int(11) NOT NULL,
+  `thoigiantao` datetime NOT NULL DEFAULT current_timestamp(),
+  `tongtien` decimal(12,0) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `masp` int(11) NOT NULL,
+  `tensp` varchar(255) NOT NULL,
+  `loai` int(11) NOT NULL,
+  `gioitinh` int(1) NOT NULL,
+  `namphathanh` varchar(50) NOT NULL,
+  `gianhap` decimal(15,0) NOT NULL,
+  `giaxuat` decimal(15,0) NOT NULL,
+  `hang` int(11) NOT NULL,
+  `mau` int(11) NOT NULL,
+  `motasp` varchar(255) NOT NULL,
+  `soluongtonkho` int(11) NOT NULL,
+  `soluongdaban` int(11) NOT NULL,
+  `trangthai` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `size`
+--
+
+CREATE TABLE `size` (
+  `masize` int(11) NOT NULL,
+  `masp` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`maadmin`);
+
+--
+-- Chỉ mục cho bảng `ctdonhang`
+--
+ALTER TABLE `ctdonhang`
+  ADD KEY `FK_ctdonhang_dh` (`madh`),
+  ADD KEY `FK_ctdonhang_sp` (`masp`),
+  ADD KEY `FK_ctdonhang_size` (`masize`);
+
+--
+-- Chỉ mục cho bảng `ctphieunhap`
+--
+ALTER TABLE `ctphieunhap`
+  ADD KEY `FK_ctphieunhap_phieunhap` (`mapn`),
+  ADD KEY `FK_ctphieunhap_sanpham` (`masp`),
+  ADD KEY `FK_ctphieunhap_size` (`masize`);
+
+--
+-- Chỉ mục cho bảng `donhang`
+--
+ALTER TABLE `donhang`
+  ADD PRIMARY KEY (`madh`),
+  ADD KEY `FK_donhang_khachhang` (`makh`);
+
+--
+-- Chỉ mục cho bảng `giohang`
+--
+ALTER TABLE `giohang`
+  ADD KEY `FK_giohang_khachhang` (`makh`),
+  ADD KEY `FK_giohang_sanpham` (`masp`);
+
+--
+-- Chỉ mục cho bảng `hang`
+--
+ALTER TABLE `hang`
+  ADD PRIMARY KEY (`mahang`);
+
+--
+-- Chỉ mục cho bảng `hinhanh`
+--
+ALTER TABLE `hinhanh`
+  ADD PRIMARY KEY (`mahinhanh`);
+
+--
+-- Chỉ mục cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`makh`);
+
+--
+-- Chỉ mục cho bảng `loai`
+--
+ALTER TABLE `loai`
+  ADD PRIMARY KEY (`maloai`);
+
+--
+-- Chỉ mục cho bảng `mau`
+--
+ALTER TABLE `mau`
+  ADD PRIMARY KEY (`mamau`);
+
+--
+-- Chỉ mục cho bảng `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  ADD PRIMARY KEY (`mancc`);
+
+--
+-- Chỉ mục cho bảng `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD PRIMARY KEY (`mapn`),
+  ADD KEY `FK_phieunhap_admin` (`maadmin`),
+  ADD KEY `FK_phieunhap_nhacungcap` (`mancc`);
+
+--
+-- Chỉ mục cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`masp`);
+
+--
+-- Chỉ mục cho bảng `size`
+--
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`masize`,`masp`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `maadmin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `donhang`
+--
+ALTER TABLE `donhang`
+  MODIFY `madh` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `hang`
+--
+ALTER TABLE `hang`
+  MODIFY `mahang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `hinhanh`
+--
+ALTER TABLE `hinhanh`
+  MODIFY `mahinhanh` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  MODIFY `makh` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `loai`
+--
+ALTER TABLE `loai`
+  MODIFY `maloai` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `mau`
+--
+ALTER TABLE `mau`
+  MODIFY `mamau` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  MODIFY `mancc` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  MODIFY `mapn` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  MODIFY `masp` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `ctdonhang`
+--
+ALTER TABLE `ctdonhang`
+  ADD CONSTRAINT `FK_ctdonhang_dh` FOREIGN KEY (`madh`) REFERENCES `donhang` (`madh`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ctdonhang_size` FOREIGN KEY (`masize`) REFERENCES `size` (`masize`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ctdonhang_sp` FOREIGN KEY (`masp`) REFERENCES `sanpham` (`masp`) ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `ctphieunhap`
+--
+ALTER TABLE `ctphieunhap`
+  ADD CONSTRAINT `FK_ctphieunhap_phieunhap` FOREIGN KEY (`mapn`) REFERENCES `phieunhap` (`mapn`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ctphieunhap_sanpham` FOREIGN KEY (`masp`) REFERENCES `sanpham` (`masp`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ctphieunhap_size` FOREIGN KEY (`masize`) REFERENCES `size` (`masize`) ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `donhang`
+--
+ALTER TABLE `donhang`
+  ADD CONSTRAINT `FK_donhang_khachhang` FOREIGN KEY (`makh`) REFERENCES `khachhang` (`makh`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `giohang`
+--
+ALTER TABLE `giohang`
+  ADD CONSTRAINT `FK_giohang_khachhang` FOREIGN KEY (`makh`) REFERENCES `khachhang` (`makh`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_giohang_sanpham` FOREIGN KEY (`masp`) REFERENCES `sanpham` (`masp`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `phieunhap`
+--
+ALTER TABLE `phieunhap`
+  ADD CONSTRAINT `FK_phieunhap_admin` FOREIGN KEY (`maadmin`) REFERENCES `admin` (`maadmin`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_phieunhap_nhacungcap` FOREIGN KEY (`mancc`) REFERENCES `nhacungcap` (`mancc`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
