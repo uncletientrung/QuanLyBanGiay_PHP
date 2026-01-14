05d2f59aed555504fc52ca95a26e446b7831d455
uncletientrung <thihachcf@gmail.com>
2026-01-14 19:20:24 +0700

    boi title

--- app/views/partials/test.php
+++ app/views/partials/test.php
@@ -4,7 +4,46 @@ require './config/connectdb.php';
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- <head>
+    <meta charset="UTF-8">
+    <title>ShoeGalaxy - Vũ trụ giày</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= ROOT_URL ?>public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;700;800&display=swap" rel="stylesheet">
+</head>
 
+<body>
+    <header class="header">
+        <div class="container header__container">
+
+            <div class="logo">
+                <img src="<?= ROOT_URL ?>public/images/logo.jpeg" alt="ShoeGalaxy">
+            </div>
+
+            <nav class="nav">
+                <ul class="nav__menu">
+                    <li><a href="<?= ROOT_URL ?>">Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">⚡ Sản phẩm</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                    <li><a href="#">Kiểm tra đơn hàng</a></li>
+                </ul>
+            </nav>
+
+            <div class="header__icons">
+                <i class="bi bi-search"></i>
+                <i class="bi bi-person"></i>
+                <div class="cart">
+                    <i class="bi bi-cart-plus"></i>
+                    <span class="badge">3</span>
+                </div>
+            </div>
+
+        </div>
+    </header>
+</body> -->
 
 <head>
   <meta http-equiv="origin-trial" content="A7vZI3v+Gz7JfuRolKNM4Aff6zaGuT7X0mf3wtoZTnKv6497cVMnhy03KDqX7kBz/q/iidW7srW31oQbBt4VhgoAAACUeyJvcmlnaW4iOiJodHRwczovL3d3dy5nb29nbGUuY29tOjQ0MyIsImZlYXR1cmUiOiJEaXNhYmxlVGhpcmRQYXJ0eVN0b3JhZ2VQYXJ0aXRpb25pbmczIiwiZXhwaXJ5IjoxNzU3OTgwODAwLCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ==">
