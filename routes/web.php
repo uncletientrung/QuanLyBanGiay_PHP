272e73eeadaddc234a3ed5c61c16f60e8e08b3f1
uncletientrung <thihachcf@gmail.com>
2026-01-22 23:46:27 +0700

    tach file xong

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); // chỉ trả về vd /QuanLyBanGiay_Php/products
 $uri = str_replace(APP_PATH, '', $uri);
-if ($uri == '' || $uri == '/' || $uri == 'index.php') {
+
+if ($uri == '' || $uri == '/' || $uri == 'dashboard') {
     require VIEW_PATH_DIR . 'partials/header.php';
     require VIEW_PATH_DIR . 'home.php';
     require VIEW_PATH_DIR . 'partials/footer.php';
@@ -10,6 +11,24 @@ if ($uri == '' || $uri == '/' || $uri == 'index.php') {
 if ($uri == 'products') {
     require VIEW_PATH_DIR . 'partials/header.php';
     require VIEW_PATH_DIR . 'products.php';
+    require VIEW_PATH_DIR . 'partials/footer.php';
+    exit;
+}
+if ($uri == 'cart') {
+    require VIEW_PATH_DIR . 'partials/header.php';
+    require VIEW_PATH_DIR . 'cart.php';
+    require VIEW_PATH_DIR . 'partials/footer.php';
+    exit;
+}
+if ($uri == 'checkout') {
+    require VIEW_PATH_DIR . 'partials/header.php';
+    require VIEW_PATH_DIR . 'checkout.php';
+    require VIEW_PATH_DIR . 'partials/footer.php';
+    exit;
+}
+if ($uri == 'contact') {
+    require VIEW_PATH_DIR . 'partials/header.php';
+    require VIEW_PATH_DIR . 'contact.php';
     require VIEW_PATH_DIR . 'partials/footer.php';
     exit;
 }
