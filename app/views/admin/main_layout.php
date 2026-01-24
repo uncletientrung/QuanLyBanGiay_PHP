681e6ecbbb86c08986361faa808f19832cb3b177
vu-tm <truongm.vuu@gmail.com>
2026-01-24 07:38:40 +0700

    fix: sửa đường dẫn

--- app/views/admin/main_layout.php
+++ app/views/admin/main_layout.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/inc/_global/config.php';
 require_once __DIR__ . '/inc/backend/config.php';
-$dm->assets_folder = '/QuanLyBanGiay_PHP/public/admin';
+$dm->assets_folder = APP_PATH . 'public';
 
 require __DIR__ . '/inc/_global/views/head_start.php';
 require __DIR__ . '/inc/_global/views/head_end.php';
