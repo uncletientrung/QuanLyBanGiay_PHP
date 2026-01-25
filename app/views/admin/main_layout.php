e8f94e1834490598e5c690b9000cf1a43cbaef58
vu-tm <truongm.vuu@gmail.com>
2026-01-25 18:26:02 +0700

    rut gon

--- app/views/admin/main_layout.php
+++ app/views/admin/main_layout.php
@@ -4,15 +4,6 @@ require_once __DIR__ . '/inc/backend/config.php';
 
 $dm->assets_folder = '../public';
 
-$dm->inc_sidebar      = __DIR__ . '/inc/backend/views/inc_sidebar.php';
-$dm->inc_header       = __DIR__ . '/inc/backend/views/inc_header.php';
-$dm->inc_footer       = __DIR__ . '/inc/backend/views/inc_footer.php';
-$dm->inc_side_overlay = __DIR__ . '/inc/backend/views/inc_side_overlay.php';
-
-$dm->l_sidebar_visible_desktop = true;
-$dm->l_sidebar_visible_mobile  = false;
-$dm->l_header_fixed            = true;
-
 require __DIR__ . '/inc/_global/views/head_start.php';
 require __DIR__ . '/inc/_global/views/head_end.php';
 require __DIR__ . '/inc/_global/views/page_start.php';
--- app/views/admin/simple_layout.php
+++ app/views/admin/simple_layout.php
@@ -1,17 +1,7 @@
 <?php
 require_once __DIR__ . '/inc/_global/config.php';
-require_once __DIR__ . '/inc/backend/config.php';
 
 $dm->assets_folder = '../public';
-
-$dm->inc_sidebar      = '';
-$dm->inc_header       = '';
-$dm->inc_footer       = '';
-$dm->inc_side_overlay = '';
-
-$dm->l_sidebar_visible_desktop = false;
-$dm->l_sidebar_visible_mobile  = false;
-$dm->l_sidebar_mini            = false;
 
 require __DIR__ . '/inc/_global/views/head_start.php';
 require __DIR__ . '/inc/_global/views/head_end.php';
