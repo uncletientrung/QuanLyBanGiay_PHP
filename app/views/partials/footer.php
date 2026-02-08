21ebb6a87cb64b8b8219219f9c3aefd9e55c2184
thuanstealer <nmthuan645@gmail.com>
2026-02-08 20:54:01 +0700

    footer

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -5,8 +5,8 @@
         <div class="row g-4">
           <div class="col-lg-3">
             <a href="#">
-              <h1 class="text-primary mb-0">Fruitables</h1>
-              <p class="text-secondary mb-0">Fresh products</p>
+              <h1 class="text-primary mb-0">GalaxyShoes</h1>
+              <p class="text-secondary mb-0">Best choice</p>
             </a>
           </div>
           <div class="col-lg-6">
@@ -14,7 +14,7 @@
               <input class="form-control border-0 w-100 py-3 px-4 rounded-pill" type="number" placeholder="Your Email">
               <button type="submit"
                 class="btn btn-primary border-0 border-secondary py-3 px-4 position-absolute rounded-pill text-white"
-                style="top: 0; right: 0;">Subscribe Now</button>
+                style="top: 0; right: 0;">Đăng ký ngay!</button>
             </div>
           </div>
           <div class="col-lg-3">
@@ -34,41 +34,40 @@
       <div class="row g-5">
         <div class="col-lg-3 col-md-6">
           <div class="footer-item">
-            <h4 class="text-light mb-3">Why People Like us!</h4>
-            <p class="mb-4">typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the like Aldus PageMaker including of Lorem Ipsum.</p>
-            <a href="" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Read More</a>
+            <h4 class="text-light mb-3">Tại sao nên chọn chúng tôi?</h4>
+            <p class="mb-4">Có tâm ắc sẽ có tầm, với những sản phẩm chất lượng cao cùng với đó là giá cả hợp lý, chúng tôi hứa hẹn sẽ mang lại dịch vụ tốt nhất cho bạn!</p>
+            <a href="<?= ROOT_URL ?>about-us" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Xem thêm</a>
           </div>
         </div>
         <div class="col-lg-3 col-md-6">
           <div class="d-flex flex-column text-start footer-item">
-            <h4 class="text-light mb-3">Shop Info</h4>
-            <a class="btn-link" href="<?= ROOT_URL ?>about-us">About Us</a>
-            <a class="btn-link" href="<?= ROOT_URL ?>contact-us">Contact Us</a>
-            <a class="btn-link" href="<?= ROOT_URL ?>privacy-policy">Privacy Policy</a>
-            <a class="btn-link" href="<?= ROOT_URL ?>terms-condition">Terms & Condition</a>
-            <a class="btn-link" href="<?= ROOT_URL ?>return-policy">Return Policy</a>
-            <a class="btn-link" href="">FAQs & Help</a>
+            <h4 class="text-light mb-3">Chính sách</h4>
+            <a class="btn-link" href="<?= ROOT_URL ?>about-us">Chính sách bảo mật thông tin</a>
+            <a class="btn-link" href="<?= ROOT_URL ?>contact-us">Chính sách thanh toán</a>
+            <a class="btn-link" href="<?= ROOT_URL ?>privacy-policy">Chính sách vận chuyển</a>
+            <a class="btn-link" href="<?= ROOT_URL ?>terms-condition">Chính sách kiểm hàng</a>
+            <a class="btn-link" href="<?= ROOT_URL ?>return-policy">Chính sách đổi trả</a>
+            <a class="btn-link" href="">Chính sách bảo hành</a>
           </div>
         </div>
         <div class="col-lg-3 col-md-6">
           <div class="d-flex flex-column text-start footer-item">
-            <h4 class="text-light mb-3">Account</h4>
-            <a class="btn-link" href="">My Account</a>
-            <a class="btn-link" href="">Shop details</a>
-            <a class="btn-link" href="">Shopping Cart</a>
-            <a class="btn-link" href="">Wishlist</a>
-            <a class="btn-link" href="">Order History</a>
-            <a class="btn-link" href="">International Orders</a>
+            <h4 class="text-light mb-3">Hướng dẫn</h4>
+            <a class="btn-link" href="">Hướng dẫn order</a>
+            <a class="btn-link" href="">Hướng dẫn mua hàng</a>
+            <a class="btn-link" href="">Điều khoản dịch vụ</a>
+            <a class="btn-link" href="<?= ROOT_URL ?>contact-us">Liên hệ</a>
+            <a class="btn-link" href="<?= ROOT_URL ?>about-us">Giới thiệu</a>
+            <a class="btn-link" href="<?= ROOT_URL ?>products">Tất cả sản phẩm</a>
           </div>
         </div>
         <div class="col-lg-3 col-md-6">
           <div class="footer-item">
-            <h4 class="text-light mb-3">Contact</h4>
-            <p>Address: 1429 Netus Rd, NY 48247</p>
-            <p>Email: Example@gmail.com</p>
-            <p>Phone: +0123 4567 8910</p>
-            <p>Payment Accepted</p>
+            <h4 class="text-light mb-3">Liên hệ</h4>
+            <p>Địa chỉ: 273 An Dương Vương, P.5, Q.5, TP.HCM</p>
+            <p>Email: galaxyshoes@gmail.com</p>
+            <p>Phone: +84 0909 669 035</p>
+            <p>Phương thức thanh toán</p>
             <img src="public/img/payment.png" class="img-fluid" alt="">
           </div>
         </div>
