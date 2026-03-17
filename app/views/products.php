79ec79ff8fdb066a7a0419f4843cc0f10d714749
thuanstealer <nmthuan645@gmail.com>
2026-03-17 12:05:00 +0700

    mot chut css

--- app/views/products.php
+++ app/views/products.php
@@ -2,7 +2,7 @@
 
 <!-- Single Page Header start -->
 <div class="container-fluid page-header py-5">
-  <h1 class="text-center text-white display-6">Shop</h1>
+  <h1 class="text-center text-white display-6">Giày</h1>
   <ol class="breadcrumb justify-content-center mb-0">
     <li class="breadcrumb-item"><a href="<?= ROOT_URL ?>">Trang chủ</a></li>
     <li class="breadcrumb-item active text-white">Shop</li>
