044857932066f83694ed6b819b206d69a9acba6d
thuanstealer <nmthuan645@gmail.com>
2026-03-20 21:06:25 +0700

    sua nav bar

--- public/js/track-order.js
+++ public/js/track-order.js
@@ -3,28 +3,25 @@
   const noOrders = document.getElementById("no-orders");
   function filterOrders(status) {
     let OrderDetail_HienThi = 0;
-    cards.forEach(card => {
+    cards.forEach((card) => {
       const orderStatus = card.dataset.status;
       if (status === "all" || orderStatus == status) {
         card.parentElement.style.display = "block";
         OrderDetail_HienThi++;
-      }
-      else {
+      } else {
         card.parentElement.style.display = "none";
       }
     });
     if (OrderDetail_HienThi === 0) {
       noOrders.classList.remove("d-none");
-    }
-    else {
+    } else {
       noOrders.classList.add("d-none");
     }
-
   }
   document.getElementById("all-tab").onclick = () => filterOrders("all");
   document.getElementById("processing-tab").onclick = () => filterOrders("0");
   document.getElementById("shipping-tab").onclick = () => filterOrders("1");
   document.getElementById("delivered-tab").onclick = () => filterOrders("2");
   document.getElementById("cancelled-tab").onclick = () => filterOrders("-1");
-
+  filterOrders("all");
 })();
